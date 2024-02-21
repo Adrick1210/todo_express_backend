94a1c8ad1d3d57cc112507f9fdafddd6fe858314
Christian Alexander <christian.alexanderato@gmail.com>
2024-02-20 20:30:01 -0800

    server.js complete

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 // DEPENDENCIES
+require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const PORT = process.env.PORT;
 
 // APP OBJECT
 const app = express();
 
 // MIDDLEWARE
+app.use(morgan("dev"));
+app.use(cors());
 
-//ROUTER
+//ROUTES
 
 // TEST
+app.get("/", (req, res) => {
+  res.send("Ready for todos");
+});
 
 // LISTENER
+app.listen(PORT, () => console.log(`Todos are coming on Port ${PORT}`));
