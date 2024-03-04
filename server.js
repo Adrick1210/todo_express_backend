9fb6f44ee2e20e5ffcf99c4ae7fbea235fb93884
Christian Alexander <christian.alexanderato@gmail.com>
2024-03-03 18:29:34 -0800

    user auth updated and working

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const PORT = process.env.PORT;
-const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs")
+const TodoRouter = require("./controllers/todo");
+const User = require("./models/User")
+// const UserRouter = require("./controllers/user");
 
 // APP OBJECT
 const app = express();
@@ -14,11 +17,37 @@ const app = express();
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-const TodoRouter = require("./controllers/todo");
-
 
 //ROUTER
 app.use("/todos", TodoRouter);
+
+// auth routes 
+//Signup
+app.post("/signup", async (req, res) => {
+  try {
+    let { username, password } = req.body;
+    password = await bcrypt.hash(password, await bcrypt.genSalt(10));
+    const user = await User.create({ username, password });
+    // console.log(user);
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ err });
+  }
+});
+
+// login
+app.post("/login", async (req, res) => {
+  let { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    return res.status(401).json({ error: "Invalid Credentials" });
+  }
+  const token = jwt.sign({ userId: user.id }, process.env.SECRET, {
+    expiresIn: "1h",
+  });
+  res.json({ token });
+});
 
 // TEST
 app.get("/", (req, res) => {
